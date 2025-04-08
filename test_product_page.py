297d252882759259d1fc96e10e0de3ecec12de8f
egorvania1 <egorvania1@gmail.com>
2025-04-08 15:17:16 +0500

    Подправил неверную маркировку теста

--- test_product_page.py
+++ test_product_page.py
@@ -62,7 +62,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.empty_basket
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link, 0)
